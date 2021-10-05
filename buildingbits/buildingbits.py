2a6ad469e7bb2531ded542f87e57a91b2649589e
Guillaume Florent <florentsailing@gmail.com>
2021-10-05 15:37:12 +0200

    First attempt

---
+++ buildingbits/buildingbits.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+r"""Buildingbits script."""
+
+from os.path import basename, dirname
+import urllib.request
+from jinja2 import Environment, FileSystemLoader
+
+URL = "https://raw.githubusercontent.com/guillaume-florent/buildingbits/main"
+FILES_URL = f"{URL}/files"
+DOCKERFILEBITS_URL = f"{URL}/dockerfilebits"
+DOCKERFILEBITS_URL = f"{URL}/gitignorebits"
+
+PROSPECTOR_YAML_FILENAME = ".prospector.yaml"
+PROSPECTOR_YAML_URL = f"{FILES_URL}/{PROSPECTOR_YAML_FILENAME}"
+
+SETUP_PY_FILENAME = "setup.py"
+SETUP_PY_URL = f"{FILES_URL}/{SETUP_PY_FILENAME}"
+
+MAKEFILE_TEMPLATE_FILENAME = "Makefile.template"
+MAKEFILE_TEMPLATE_URL = f"{FILES_URL}/{MAKEFILE_TEMPLATE_FILENAME}"
+
+SUPPORTED_DOCKERFILE_KEYS = ["base", "miniconda", "occ_0.18", "occ_7.5", "common_infrastructure"]
+SUPPORTED_GITIGNORE_KEYS = ["generic", "python", "pycharm", "pytest", "pytest-cov", "pytest-benchmark", "mypy"]
+
+
+def download(url: str, file_name: str) -> None:
+    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        data = response.read()  # a `bytes` object
+        out_file.write(data)
+
+
+def file_from_template(keys, url, template_name, output_file):
+    # 1 - List files in dockerfilebits
+    # 2 - for each file, create a dict with the file name (without extension) as a key, the file content as value
+    # 3 - Use the dict to template the Dockerfile.template
+    dict = {}
+    for k in keys:
+        dict[k] = urllib.request.urlopen(f"{url}/{k}.txt").read().decode("utf-8")
+
+    file_loader = FileSystemLoader('.')  # directory of template file
+    env = Environment(loader=file_loader)
+
+    template = env.get_template(template_name)  # load template file
+    output = template.render(**dict)
+    with open(output_file, 'w') as file_:
+        file_.write(output)
+
+
+if __name__ == "__main__":
+    # download .prospector.yaml
+    download(PROSPECTOR_YAML_URL, PROSPECTOR_YAML_FILENAME)
+
+    # download setup.py
+    download(SETUP_PY_URL, SETUP_PY_FILENAME)
+
+    # download Makefile.template
+    download(MAKEFILE_TEMPLATE_URL, MAKEFILE_TEMPLATE_FILENAME)
+
+    # create Dockerfile.bck from local template
+    file_from_template(keys=SUPPORTED_DOCKERFILE_KEYS,
+                       url=DOCKERFILEBITS_URL,
+                       template_name="Dockerfile.template",
+                       output_file="Dockerfile")
+
+    # create .gitignore from local template
+    file_from_template(keys=SUPPORTED_GITIGNORE_KEYS,
+                       url=GITIGNOREBITS_URL,
+                       template_name="gitignore.template",
+                       output_file=".gitignore")
+
+    # overwrite the initial Makefile
+    file_loader = FileSystemLoader('.')  # directory of template file
+    env = Environment(loader=file_loader)
+
+    template = env.get_template("Makefile.template")  # load template file
+    output = template.render(project_name=basename(dirname(__file__)))
+    with open("Makefile", 'w') as file_:
+        file_.write(output)
+
