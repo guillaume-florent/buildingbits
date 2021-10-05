6830d000b24ea67642d5f559e8f76fdca2398cc3
Guillaume Florent <florentsailing@gmail.com>
2021-10-05 17:05:05 +0200

    propector errors non blocking, clean in makefile, buildingbits.py linting

--- buildingbits/buildingbits.py
+++ buildingbits/buildingbits.py
@@ -22,34 +22,35 @@ MAKEFILE_TEMPLATE_FILENAME = "Makefile.template"
 MAKEFILE_TEMPLATE_URL = f"{FILES_URL}/{MAKEFILE_TEMPLATE_FILENAME}"
 
 SUPPORTED_DOCKERFILE_KEYS = ["base", "miniconda", "occ_0.18", "occ_7.5", "common_infrastructure"]
-SUPPORTED_GITIGNORE_KEYS = ["generic", "python", "pycharm", "pytest", "pytest_cov", "pytest_benchmark", "mypy"]
+SUPPORTED_GITIGNORE_KEYS = ["generic", "python", "pycharm", "pytest", "pytest_cov",
+                            "pytest_benchmark", "mypy"]
 
 
 def trace(msg):
+    r"""Print a trace."""
     print(msg)
 
 
 def download(url: str, file_name: str) -> None:
+    r"""Download a file."""
     with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
         data = response.read()  # a `bytes` object
         out_file.write(data)
 
 
 def file_from_template(keys, url, template_name, output_file):
-    # 1 - List files in dockerfilebits
-    # 2 - for each file, create a dict with the file name (without extension) as a key, the file content as value
-    # 3 - Use the dict to template the Dockerfile.template
-    dict = {}
+    r"""Create a file using the definitions in url + key and the provided template."""
+    d = {}
     for k in keys:
         trace(f"Reading : {url}/{k}.txt")
-        dict[k] = urllib.request.urlopen(f"{url}/{k}.txt").read().decode("utf-8")
+        d[k] = urllib.request.urlopen(f"{url}/{k}.txt").read().decode("utf-8")
 
     file_loader = FileSystemLoader('.')  # directory of template file
     env = Environment(loader=file_loader)
 
     template = env.get_template(template_name)  # load template file
-    output = template.render(**dict)
-    with open(output_file, 'w') as file_:
+    output = template.render(**d)
+    with open(output_file, 'w', encoding="utf-8") as file_:
         file_.write(output)
 
 
@@ -94,7 +95,6 @@ if __name__ == "__main__":
     project_name = basename(os.getcwd())
     trace(f"Project name: {project_name}")
     output = template.render(project_name=project_name)
-    with open("Makefile", 'w') as file_:
+    with open("Makefile", 'w', encoding="utf-8") as file_:
         file_.write(output)
     trace(" ... done.")
-
