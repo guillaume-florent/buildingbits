b4f2493ec1d099ef30a6fcd31343beef0207da8e
Guillaume Florent <florentsailing@gmail.com>
2021-10-05 13:57:52 +0200

    Initial commit

---
+++ files/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+r"""Generic setup.py. Present for legacy installs, use pyproject.toml and setup.cfg instead"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
